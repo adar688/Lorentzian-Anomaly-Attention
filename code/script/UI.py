de23adf214142aa99a3fdb49955b65131aebcba2
Adar Budomski <adar688@gmail.com>
2025-12-12 15:48:46 +0200

    /

--- code/script/UI.py
+++ code/script/UI.py
@@ -11,7 +11,7 @@ import gradio as gr
 # -----------------------
 
 # Script names: change if your files are in another folder or have other names
-PREPARE_SCRIPT = "utils/prepareData.py"
+PREPARE_SCRIPT = "script/utils/prepareData.py"
 RUN_SCRIPT = "simple_main.py"
 
 # Folder that contains generated plots
